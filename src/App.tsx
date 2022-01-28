99bf7fcd8246ea20e5b8b68a1ae7d3330233c9ec
ShahmirAli2646 <shahmirali2646@gmail.com>
2022-01-28 13:59:03 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="app">
+      
       <h1 className="center fs-30">React Invoice Generator</h1>
       <Routes>
         <Route path="/react-invoice-generator" element={ <InvoicePage />}/>
